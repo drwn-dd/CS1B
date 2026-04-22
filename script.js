0d036822cf112065d7a3ccfebbc653f2ed70cc40
drwn-dd <daigodarwin185@gmail.com>
2026-04-22 09:53:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function showSection(sectionId) {
     document.getElementById("home").style.display = "none";
+    document.getElementById("dashboard").style.display = "none";
     document.getElementById("quiz").style.display = "none";
     document.getElementById("laboratories").style.display = "none";
     document.getElementById("exam").style.display = "none";
 
     document.getElementById(sectionId).style.display = "block";
+
+    if (sectionId === "dashboard") {
+        updateDashboard();
+    }
 }
 
 function goHome() {
     document.getElementById("home").style.display = "block";
+    document.getElementById("dashboard").style.display = "none";
     document.getElementById("quiz").style.display = "none";
     document.getElementById("laboratories").style.display = "none";
     document.getElementById("exam").style.display = "none";
@@ -22,4 +28,16 @@ function toggleMenu(el) {
 function closeMenu() {
     document.querySelector(".burger").classList.remove("active");
     document.getElementById("menu").classList.add("hidden");
+}
+
+/* DASHBOARD COUNTER */
+function updateDashboard() {
+    let quiz = document.querySelectorAll("#quiz .box img").length;
+    let lab = document.querySelectorAll("#laboratories .box img").length;
+    let exam = document.querySelectorAll("#exam .box img").length;
+
+    document.getElementById("quizCount").innerText = quiz;
+    document.getElementById("labCount").innerText = lab;
+    document.getElementById("examCount").innerText = exam;
+    document.getElementById("totalCount").innerText = quiz + lab + exam;
 }
